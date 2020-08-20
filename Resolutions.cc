6878cde5dd8b7f3b9c87390edff71ae4db0a7797
KathrynColdham <kathryn.coldham@cern.ch>
2020-08-20 18:09:33 +0200

    Outputting more info into the text file.

--- Resolutions.cc
+++ Resolutions.cc
@@ -2,23 +2,14 @@ using namespace ROOT;
 
 
 vector<float> HitResolutionVector;
+vector<float> DoubleDifferenceVector;
+vector<float> HitDXVector;
+vector<float> TrackDXVector;
 
 void ResolutionsCalculator(const string& region){
 
   //opening the root file
   ROOT::RDataFrame d("anResol/reso", "hitresol.root");
-
-  //Filtering the RDataFrame to consider each tracker region
-
-  /*uint32_t detID1 = ...;
-    auto subdet = (detID1>>25)&0x7; // TIB = 3, TID = 4, TOB = 5, TEC = 6
-    for TIB and TOB: auto layerBarrel = (detID1>>14)&0x7;
-    for TID: auto sideTID = (detID1>>13)&0x3;
-	     auto wheelTID = (detID1>>11)&0x3;
-	     auto ringTID = (detID1>>9)&0x3;
-    for TEC: auto sideTEC = (detID1>>18)&0x3;
-	     auto wheelTEC = (detID1>>14)&0xF;
-  	     auto ringTEC = (detID1>>5)&0x7;*/
 
   //Layers 1-4 of the TIB
   auto SubDet_TIB_L1_Function{[](const unsigned int& detID1_input){return (((detID1_input>>25)&0x7) == 3) && (((detID1_input>>14)&0x7) == 1);}};
@@ -45,8 +36,9 @@ void ResolutionsCalculator(const string& region){
   auto SubDet_WheelTEC_Function{[](const unsigned int& detID1_input){return (((detID1_input>>25)&0x7) == 6) && ((detID1_input>>14)&0xF);}};
   auto SubDet_RingTEC_Function{[](const unsigned int& detID1_input){return (((detID1_input>>25)&0x7) == 6) && ((detID1_input>>5)&0x7);}};
 
-  auto dataframe = d.Filter("detID1"); //this is just to define dataframe and is overwritten in the if else statements below
 
+  //Filtering the RDataFrame to consider each tracker region
+  auto dataframe = d.Filter("detID1"); //this is just to define dataframe and is overwritten in the if else statements below
 
   if(region == "TIB_L1"){auto dataframe = d.Filter(SubDet_TIB_L1_Function, {"detID1"});} 
   else if(region == "TIB_L2"){auto dataframe = d.Filter(SubDet_TIB_L2_Function, {"detID1"});}
@@ -75,22 +67,31 @@ void ResolutionsCalculator(const string& region){
 
   auto HistoName_DoubleDiff = "DoubleDifference_" + region;
   auto HistoName_HitDX = "HitDX_" + region;
+  auto HistoName_TrackDX = "TrackDX_" + region;
 
-  auto h_DoubleDifference = dataframe.Define(HistoName_DoubleDiff, DifferenceFunction, {"hitDX", "trackDX"}).Histo1D(HistoName_DoubleDiff); 
+  auto h_DoubleDifference = dataframe.Define(HistoName_DoubleDiff, DifferenceFunction, {"hitDX", "trackDX"}).Histo1D({HistoName_DoubleDiff.c_str(), HistoName_DoubleDiff.c_str(), 40, -10, 10}, HistoName_DoubleDiff); 
   auto h_hitDX = dataframe.Define(HistoName_HitDX, {"hitDX"}).Histo1D(HistoName_HitDX);
+  auto h_trackDX = dataframe.Define(HistoName_TrackDX, {"trackDX"}).Histo1D(HistoName_TrackDX);
 
   //Applying gaussian fits, taking the resolutions and squaring them
   h_DoubleDifference->Fit("gaus");
   h_hitDX->Fit("gaus");
+  h_trackDX->Fit("gaus");
   auto hitDX_trackDX_StdDev = h_DoubleDifference->GetStdDev();
-  auto hitDX_StdDev = h_hitDX->GetStdDev(); 
+  auto hitDX_StdDev = h_hitDX->GetStdDev();
+  auto trackDX_StdDev = h_trackDX->GetStdDev();
   auto sigma2_PredMinusMeas = pow(hitDX_trackDX_StdDev, 2);
   auto sigma2_Meas = pow(hitDX_StdDev, 2);
- 
+  auto sigma2_Pred = pow(trackDX_StdDev, 2); 
+
+  DoubleDifferenceVector.push_back(sigma2_PredMinusMeas);
+  HitDXVector.push_back(sigma2_Meas);
+  TrackDXVector.push_back(sigma2_Pred);
 
   //Saving the histograms with gaussian fits applied to an output root file
   h_DoubleDifference->Write();
   h_hitDX->Write();
+  h_trackDX->Write();
 
   //Calculating the hit resolution
   //sigma2_PredMinusMeas - sigma2_Meas;
@@ -109,27 +110,36 @@ void Resolutions(){
 
   TFile * output = new TFile("GaussianFits.root", "RECREATE");
 
-  ResolutionsCalculator("TIB_L1");
-  ResolutionsCalculator("TIB_L2");
-  ResolutionsCalculator("TIB_L3");
-  ResolutionsCalculator("TIB_L4");
-  ResolutionsCalculator("Side_TID");
-  ResolutionsCalculator("Wheel_TID");
-  ResolutionsCalculator("Ring_TID");
-  ResolutionsCalculator("TOB_L1");
-  ResolutionsCalculator("TOB_L2");
-  ResolutionsCalculator("TOB_L3");
-  ResolutionsCalculator("TOB_L4");
-  ResolutionsCalculator("TOB_L5");
-  ResolutionsCalculator("TOB_L6");
-  ResolutionsCalculator("Side_TEC");
-  ResolutionsCalculator("Wheel_TEC");
-  ResolutionsCalculator("Ring_TEC");
+  vector<std::string> LayerNames = {"TIB_L1",   "TIB_L2",    "TIB_L3",    "TIB_L4",
+				    "Side_TID", "Wheel_TID", "Ring_TID",  "TOB_L1",
+				    "TOB_L2",   "TOB_L3",    "TOB_L4",    "TOB_L5",
+				    "TOB_L6",   "Side_TEC",  "Wheel_TEC", "Ring_TEC"};
+
 
   std::ofstream HitResoTextFile;
   HitResoTextFile.open("HitResolutionValues.txt");
 
-  for(int i = 0; i < HitResolutionVector.size(); i++){HitResoTextFile << HitResolutionVector.at(i) << endl;}
+  //auto Width = 0;
+  auto Width = 25; 
+
+  for(int i = 0; i < LayerNames.size(); i++){ResolutionsCalculator(LayerNames.at(i));}
+ 
+  HitResoTextFile << "Layer " << std::setw(Width) << " Resolution " << std::setw(Width) << " HitDX " << std::setw(Width) << " sigma2_trackDX " << std::setw(Width) << " DoubleDifference " << std::endl;
+
+  for(int i = 0; i < HitResolutionVector.size(); i++){
+
+
+        if(LayerNames.at(i) == "Wheel_TID" || LayerNames.at(i) == "Wheel_TEC"){
+		Width = 22;
+	}
+	else if(LayerNames.at(i) == "Side_TID" || LayerNames.at(i) == "Side_TEC" || LayerNames.at(i) == "Ring_TID" || LayerNames.at(i) == "Ring_TEC"){
+		Width = 23;
+        }
+        else{Width = 25;}
+
+	HitResoTextFile << LayerNames.at(i) << std::setw(Width) << HitResolutionVector.at(i) << std::setw(Width) << HitDXVector.at(i)  << std::setw(Width) << TrackDXVector.at(i) << std::setw(Width) << DoubleDifferenceVector.at(i) << endl;
+
+  }
 
   output->Close();
 
